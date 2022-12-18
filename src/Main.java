6c9143d98c0e310049ad1115d48cdcebc5f6c4eb
Al040 <klpk44@yandex.ru>
2022-12-18 13:30:19 +0300

    Правка замечаний

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
+import java.time.Year;
+
 public class Main {
     public static void main(String[] args) {
         int salary = 15000;
         int total = 0;
         int a = 0;
-        while ( total < 2_459_000) {
-            total =total+total/100;
-            total =total+salary;
-            a=a+1;
-            System.out.println("Месяц "+a+" сумма накоплений равна "+total+ " рублей" );
+        int All = 2_459_000;
+        while (total < All) {
+            total = total + total / 100;
+            total = total + salary;
+            a = a + 1;
+            System.out.println("Месяц " + a + " сумма накоплений равна " + total + " рублей");
         }
 
         int start = 1;
         while (start < 11) {
-            System.out.println (start);
-            start = start + 1 ;
+            System.out.println(start);
+            start = start + 1;
         }
 
         int stirt = 11;
-        for (; stirt > 1 ;) {
+        for (; stirt > 1; ) {
             stirt = stirt - 1;
             System.out.println(stirt);
         }
@@ -26,53 +29,61 @@ public class Main {
         int born = 17;
         int death = 8;
         int dif = born - death;
-        int next=0;
-        for (int i=1; i < 11; i++) {
-            y=y+y*dif/1000;
+        int next = 0;
+        for (int year = 1; year < 11; year++) {
+            y = y + y * dif / 1000;
 
-            System.out.println("Год "+i+" численность населения составляет "+ y);
+            System.out.println("Год " + year + " численность населения составляет " + y);
         }
 
         int In = 15000;
         int all = 0;
         int b = 0;
-        for (;all<12_000_000;b++) {
-            all = all + 7 * (all / 100);
+        int l = 12_000_000;
+        for (; all < l; b++) {
+            int g = 7 * (all / 100);
+            all = all + 7 * g;
             all = all + In;
-            if (b%6==0) {
+            if (b % 6 == 0) {
                 System.out.println("Месяц " + b + " количество накоплений " + all);
             }
         }
 
-        int sa= 15000;
-        int al= 0;
-        for (int i = 1;i<109;i++) {
+        int sa = 15000;
+        int al = 0;
+        for (int i = 1; i < 109; i++) {
             al = al + 7 * (al / 100);
             al = al + sa;
-            if(i%6==0) {
-                System.out.println("Месяц "+i+" количество наколпений "+al);
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " количество наколпений " + al);
             }
         }
 
-        int friday=2;
-        while (friday<30) {
-            friday=friday+7;
-            System.out.println("Сегодня пятница "+friday+"-е число. Необходимо подготовить отчёт");
-
+        int friday = 2;
+        while (friday <= 31) {
+            friday = friday + 7;
+            System.out.println("Сегодня пятница " + friday + "-е число. Необходимо подготовить отчёт");
 
 
         }
-
-        int c = 1817;
-        while (c<2054) {
-            c=c+79;
-            System.out.println(c);
+        int c = 2017;
+        int bef = c - 200;
+        int aft = 2054;
+        while (bef < aft) {
+            bef = bef + 79;
+            System.out.println(bef);
 
         }
-
-
-
-
+        do {
+            System.out.println(bef / 79);
+        }while (bef<aft);
 
     }
 }
+
+
+
+
+
+
+
